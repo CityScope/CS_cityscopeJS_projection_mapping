59cbb5664aae62e8aa3d4f1b25f63c69bc5d9d3c
Ariel Noyman <arielnoyman@gmail.com>
2024-04-12 12:07:40 -0400

    cleanup gh-pages

--- README.md
+++
@@ -1,3 +0,0 @@
-# CS_cityscopeJS projection mapping
-
-A tool for projection mapping on tangible interfaces, part of cityscopeJS ecosystem.
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import App from "./App";
-import { createRoot } from "react-dom/client";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(<App />);
